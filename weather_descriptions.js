aa6c1b8aebec2921664816d06880278b0e41b593
mylabernard8 <myla.bernard26@avenues.org>
2025-01-31 12:41:33 -0500

    My second change was adding weather descriptions, have to improve as there is a lot of overlapping titles

--- weather_descriptions.js
+++ weather_descriptions.js
@@ -1,1 +1,43 @@
+let weatherJSON;
+let minTemp = Infinity;
+let maxTemp = -Infinity;
+let dx;
+
+function preload() {
+  weatherJSON = loadJSON("https://api.weather.gov/gridpoints/OKX/33,37/forecast");  
+}
+
+function setup() {
+  createCanvas(600, 400);
+  dx = width / (weatherJSON.properties.periods.length + 2);
+  
+  for (const p of weatherJSON.properties.periods) {
+    minTemp = min(p.temperature, minTemp);
+    maxTemp = max(p.temperature, maxTemp);
+  }
+  noLoop();
+}
+
+function draw() {
+  background(220);
+  textSize(16);
+  textAlign(CENTER);
+  text("Weather Forecast Visualization", width / 2, 30);
+  textSize(12);
+  textAlign(LEFT);
+  
+  let px = dx;
+  let py = map(weatherJSON.properties.periods[0].temperature, minTemp, maxTemp, 0.8 * height, 0.2 * height);
+  
+  for (let i = 1; i < weatherJSON.properties.periods.length; i++) {
+    let cx = dx * (i + 1);
+    let cy = map(weatherJSON.properties.periods[i].temperature, minTemp, maxTemp, 0.8 * height, 0.2 * height);
+    line(px, py, cx, cy);
+    fill(0);
+    text(weatherJSON.properties.periods[i].temperature + "°F", cx, cy - 10);
+    text(weatherJSON.properties.periods[i].shortForecast, cx, cy + 15);
+    px = cx;
+    py = cy;
+  }
+}
 
